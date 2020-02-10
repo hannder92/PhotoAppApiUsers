96eccfdeeba30cf480c6822f3cffc76425a0466c
Johann Medina <johann.medina@valid.com>
2020-02-10 17:19:51 -0500

    first Commit

---
+++ src/main/java/com/medco/photoapp/api/users/PhotoAppApiUserApplication.java
@@ -0,0 +1,15 @@
+package com.medco.photoapp.api.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PhotoAppApiUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppApiUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medco/photoapp/api/users/PhotoAppApiUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medco.photoapp.api.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
