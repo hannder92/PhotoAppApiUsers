a359adca02bc9b814ce0515e54c391b9b3c00ef8
Johann Medina <hannder92@gmail.com>
2020-03-04 22:19:37 -0500

    04032020

--- src/main/java/com/medco/photoapp/api/users/service/impl/UserServiceImpl.java
+++ src/main/java/com/medco/photoapp/api/users/service/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.medco.photoapp.api.users.data.UserEntity;
@@ -16,21 +17,25 @@ import com.medco.photoapp.api.users.shared.UserDto;
 public class UserServiceImpl implements UserService {
 
 	private UsersRepository userRepository;
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	@Autowired
-	public UserServiceImpl(UsersRepository userRepository) {
+	public UserServiceImpl(UsersRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
 		this.userRepository = userRepository;
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
 	}
 
 	@Override
 	public UserDto createUser(UserDto userDetails) {
 		userDetails.setUserId(UUID.randomUUID().toString());
+		userDetails.setEncryptedPassword(bCryptPasswordEncoder.encode(userDetails.getPassword()));
 		ModelMapper modelMapper = new ModelMapper();
 		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 		UserEntity userEntity = modelMapper.map(userDetails, UserEntity.class);
-		userEntity.setEncryptedPassword("test");
 		userRepository.save(userEntity);
-		return null;
+
+		UserDto returnValue = modelMapper.map(userEntity, UserDto.class);
+		return returnValue;
 	}
 
 }
--- src/main/java/com/medco/photoapp/api/users/ui/controller/UserController.java
+++ src/main/java/com/medco/photoapp/api/users/ui/controller/UserController.java
@@ -6,6 +6,9 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.medco.photoapp.api.users.service.UserService;
 import com.medco.photoapp.api.users.shared.UserDto;
 import com.medco.photoapp.api.users.ui.model.CreateUserRequestModel;
+import com.medco.photoapp.api.users.ui.model.CreateUserResponseModel;
 
 @RestController
 @RequestMapping("/users")
@@ -31,15 +35,16 @@ public class UserController {
 		return "Working on port " + env.getProperty("local.server.port");
 	}
 
-	@PostMapping
-	public String createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
+	@PostMapping(consumes = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }, produces = {
+			MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+	public ResponseEntity<CreateUserResponseModel> createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
 		ModelMapper modelMapper = new ModelMapper();
 		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 
 		UserDto userDto = modelMapper.map(userDetails, UserDto.class);
-		userService.createUser(userDto);
-
-		return "Create user method is called";
+		UserDto createdUser = userService.createUser(userDto);
+		CreateUserResponseModel returnValue = modelMapper.map(createdUser, CreateUserResponseModel.class);
+		return ResponseEntity.status(HttpStatus.CREATED).body(returnValue);
 	}
 
 }
