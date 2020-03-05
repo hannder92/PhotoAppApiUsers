44e2b581e2468d62eb903810f71d0ddd891e3598
Johann Medina <hannder92@gmail.com>
2020-03-04 23:40:45 -0500

    login advance 20200304

--- src/main/java/com/medco/photoapp/api/users/service/UserService.java
+++ src/main/java/com/medco/photoapp/api/users/service/UserService.java
@@ -1,9 +1,12 @@
 package com.medco.photoapp.api.users.service;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 import com.medco.photoapp.api.users.shared.UserDto;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
 
 	UserDto createUser(UserDto userDetails);
 
+	UserDto getUSerDetailsByEmail(String email);
 }
--- src/main/java/com/medco/photoapp/api/users/service/impl/UserServiceImpl.java
+++ src/main/java/com/medco/photoapp/api/users/service/impl/UserServiceImpl.java
@@ -1,10 +1,14 @@
 package com.medco.photoapp.api.users.service.impl;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -36,6 +40,26 @@ public class UserServiceImpl implements UserService {
 
 		UserDto returnValue = modelMapper.map(userEntity, UserDto.class);
 		return returnValue;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		UserEntity userEntity = userRepository.findByEmail(username);
+
+		if (userEntity == null)
+			throw new UsernameNotFoundException(username);
+
+		return new User(userEntity.getEmail(), userEntity.getEncryptedPassword(), true, true, true, true,
+				new ArrayList<>());
+	}
+
+	@Override
+	public UserDto getUSerDetailsByEmail(String email) {
+		UserEntity userEntity = userRepository.findByEmail(email);
+
+		if (userEntity == null)
+			throw new UsernameNotFoundException(email);
+		return new ModelMapper().map(userEntity, UserDto.class);
 	}
 
 }
